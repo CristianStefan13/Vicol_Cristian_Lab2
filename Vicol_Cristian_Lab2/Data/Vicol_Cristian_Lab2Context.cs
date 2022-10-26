f981ce726729b2ced037df2651e94dae2a07cbb9
User <User@DESKTOP-457QTPS>
2022-10-27 00:25:32 +0300

    Add project files.

---
+++ Vicol_Cristian_Lab2/Data/Vicol_Cristian_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Vicol_Cristian_Lab2.Models;
+
+namespace Vicol_Cristian_Lab2.Data
+{
+    public class Vicol_Cristian_Lab2Context : DbContext
+    {
+        public Vicol_Cristian_Lab2Context (DbContextOptions<Vicol_Cristian_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vicol_Cristian_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Vicol_Cristian_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Vicol_Cristian_Lab2.Models.Author> Author { get; set; }
+    }
+}
